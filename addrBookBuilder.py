1d30ee97913ebee60233cbcf13bd1a778bdcd05f
Zheng Hao Tan <tanzhao@umich.edu>
2015-05-16 21:36:22 -0400

    initial commit

---
+++ addrBookBuilder.py
@@ -0,0 +1,9 @@
+# Name: Zheng Hao Tan
+# Email: tanzhao@umich.edu
+# Date: May 16, 2015
+
+# This program will read in web pages and extract names and email addresses and
+# and save them to a list of contacts.
+
+import re
+import sys
