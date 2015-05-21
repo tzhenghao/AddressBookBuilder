b256688b85deb927d279357c2443f7e2c4d52eda
Zheng Hao Tan <tanzhao@umich.edu>
2015-05-21 02:45:57 -0400

    Added helper functions for the phone and email parsers

--- addrBookBuilder.py
+++ addrBookBuilder.py
@@ -5,8 +5,7 @@
 # This program will read in web pages and extract names and email addresses and
 # and save them to a list of contacts.
 
-# Usage
-# TODO
+# USAGE: python3 addrBookBuilder.py <input file (optional)>')
 
 import pyperclip # For clipboard usage.
 import re # For regular expression usage.
@@ -14,39 +13,73 @@ import sys
 import os # For file streams
 
 # TODO - Handle names.
+#def nameHandler(text):
 
 # EFFECTS: Returns the phone numbers if present, None if phone number is absent.
-def phoneNumberHandler():
-	phoneReg = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-	phoneNums = phoneNums.findall()
+def phoneNumberHandler(text):
+	phoneReg = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+	phoneNums = phoneReg.findall(text)
+	return phoneNums
+
 
 # EFFECTS: Returns the email addresses if present, None if absent.
-def emailAddrHandler():
-	emailReg = re.compile(r'@+(\.com | \.edu)')
-	emails = emailReg.findall()
+def emailAddrHandler(text):
+	print('hehehoidnjwaonidwa')
+	emailReg = re.compile(r'@+(\.com|\.edu)')
+	emails = emailReg.findall(text)
+	return emails
+
 
 # REQUIRES: The data structures of the contacts to be populated.
 # EFFECTS: Prints out the contacts that are detectable by the program.
-#def printContacts():
+def printContacts():
+	print('calling printContacts...')
 
 # REQUIRES: The data structures of the contacts to be populated.
 # EFFECTS: Saves the contacts that are detectable by the program into a file.
-#def saveContacts():
+def saveContacts(output):
+	openedFile = open(output, 'w')
+	openedFile.write('lala')
+
+# Helper function.
+def phoneAndEmailHelper(sentence):
+	phoneNumber = phoneNumberHandler(sentence)
+	emailAddr = emailAddrHandler(sentence)
+
+	if len(phoneNumber) != 0:
+		print(phoneNumber)
+
+	if len(emailAddr) != 0:
+		print(emailAddr)
+
+	# Handle output to contacts.txt
+	saveContacts(OUTPUTFILE)
 
 readFromFile = False
+NUMARGS = 2
+OUTPUTFILE = 'contacts.txt'
 
-if len(sys.argv) < 2:
+# Handle command line arguments
+if len(sys.argv) < NUMARGS:
 	print('Reading from clipboard...')
-elif len(sys.argv) == 2:
+elif len(sys.argv) == NUMARGS:
 	print('Reading from ' + str(sys.argv[1]) + '...')
 	readFromFile = True
 else:
 	print('Error: Too many arguments for this program!')
+	print('USAGE: python3 addrBookBuilder.py <input file (optional)>')
 	sys.exit()
 
-# Handle read from File
+# Handle read from file
 if readFromFile:
 	openedFile = open(sys.argv[1])
 	fileContent = openedFile.readlines()
+
+	for sentence in fileContent:
+		phoneAndEmailHelper(sentence)
+
+# Handle clipboard
 else:
 	clipboardContent = pyperclip.paste()
+	phoneAndEmailHelper(clipboardContent)
+
